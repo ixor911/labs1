7ae0426d340e7c5d47062a94a2ec4a2e152096e7
ixor911 <igorbutenko9111@gmail.com>
2019-11-06 12:30:30 +0100

    final

--- fib/main.cpp
+++ fib/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-
 using namespace std;
-
 int n=0;
 void fib(){
     int a=0, b=1, x=1;
@@ -14,18 +12,10 @@ void fib(){
         n++;
         if(a==0)
             break;
-
-
-
     }
-
 }
-
 int main(){
     fib();
     cout << n;
-
-
-
     return 0;
 }
